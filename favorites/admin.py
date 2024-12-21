487ff775ff54d628b4ffe0d080733fec3b5f8b7f
sibylassana95 <sibyamara95@gmail.com>
2024-12-21 13:07:25 +0000

    mise à jour de l'interface d'administration pour l'afficher et le filtrer.

--- favorites/admin.py
+++ favorites/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import Favorite
 
 class FavoriteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'url', 'user', 'description')
+    list_display = ('title', 'url', 'user', 'description', 'created_at')
     search_fields = ('title', 'url', 'user__username')
+    list_filter = ('user', 'created_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'url', 'user')
+        }),
+        ('Details', {
+            'fields': ('description', 'created_at'),
+            'classes': ('collapse',),
+        }),
+    )
 
 admin.site.register(Favorite, FavoriteAdmin)
