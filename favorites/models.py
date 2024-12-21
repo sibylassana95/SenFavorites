599a71ebc4f046ecc4242d7fbc5f0c4c223af275
sibylassana95 <sibyamara95@gmail.com>
2024-12-21 13:07:06 +0000

    Ajout du champ 'created_at' au modèle Favorite et mise à jour de l'interface d'administration pour l'afficher et le filtrer.

--- favorites/models.py
+++ favorites/models.py
@@ -6,6 +6,7 @@ class Favorite(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
