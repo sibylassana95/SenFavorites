e0d6a1542c41f4d58e4e8d6d8f99cc2a9d68fb1d
sibylassana95 <sibyamara95@gmail.com>
2024-12-26 14:18:22 +0000

    Suppression du doublons l'importation du modèle Favorite dans views.py

--- favorites/views.py
+++ favorites/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .models import Favorite
 from .serializers import FavoriteSerializer
 from rest_framework.views import APIView
 from django.core.paginator import Paginator
